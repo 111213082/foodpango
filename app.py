f0783ed6e161fb48492a2eb27cada29a84ee7c15
DESKTOP-TSSB0Q1\SandraYang <sandra930621@gmail.com>
2024-12-31 14:32:04 +0800

    error

--- app.py
+++ app.py
@@ -272,4 +272,120 @@ def restaurant_dashboard():
 def bro_dashboard():
     return render_template('bro_dashboard.html')
 
+ #商家主畫面
+@app.route("/SellerHome")
+def seller():
+    uID = session.get('loginID')  # 取得目前登入使用者的 uID
+    dat = home(uID)
+    return render_template('SellerHome.html', data=dat)
+@app.route("/rHome/<int:rID>")
+def rHome(rID):
+    #rID = session.get('loginID')  # 取得目前登入使用者的 uID
+    isActive = get_is_active(rID)
+    categorized_food = menu_food(rID)
+    return render_template('seller/SellerHome.html', categorized_food=categorized_food, rID=rID,isActive=isActive)
 
+# 更新是否營業
+@app.route('/update_active/<int:rID>', methods=['POST'])
+def update_active(rID):
+    is_active = request.form.get('active') == '1' 
+    update_is_active(is_active, rID)
+    return redirect(url_for('rHome', rID=rID))
+
+#菜單
+@app.route('/menu/<int:rID>')
+def menu(rID):
+    categorized_food = menu_food(rID)
+    return render_template('seller/Menu.html', categorized_food=categorized_food, rID=rID)
+
+#新增餐點    
+@app.route('/addFood/<int:rID>', methods=['GET', 'POST'])
+def addFood(rID):
+    categories = get_categories_by_restaurant(rID)
+    if request.method == 'POST':
+        form = request.form
+        name = form.get('name')
+        description = form.get('description')
+        price = form.get('price')
+        category_id = form.get('ID')
+        is_vegetarian = form.get('vegetarian') == '素'
+        add(rID, name, description, price, category_id, is_vegetarian)
+        return redirect(f"/menu/{rID}")
+    return render_template('seller/AddMenu.html', categories=categories,rID=rID)
+
+# 修改菜單
+@app.route('/edit/<int:rID>/<int:fID>', methods=['GET', 'POST'])
+def edit(rID, fID):
+    if request.method == 'POST':
+        form = request.form
+        name = form.get('name')
+        description = form.get('description')
+        price = form.get('price')
+        category_id = form.get('category_id')
+        is_vegetarian = form.get('is_vegetarian') == 'on'
+        # 更新餐點資料
+        edit_food(fID, name, description, price, category_id,is_vegetarian)
+        return redirect(f"/menu/{rID}")
+
+    # 獲取當前餐點資料
+    food = get_food(fID)
+    categories = get_categories_by_restaurant(rID)
+    return render_template('seller/MenuEdit.html', food=food, categories=categories,rID=rID)
+
+# 刪除餐點
+@app.route('/delete/<int:rID>', methods=['POST'])
+def delete(rID):
+    form = request.form
+    fID = form.get('fID')  # 假設表單中有隱藏欄位傳遞 fID
+    if fID:
+        delete_food(fID)  # 執行刪除操作
+        return redirect(f"/menu/{rID}")
+    else:
+        return "未提供 fID", 400  # 如果未提供 fID，返回錯誤
+
+#訂單列表
+@app.route('/order/<int:rID>')
+def order(rID):
+    order_list = orderList(rID)
+    return render_template('seller/Order.html',order_list=order_list,rID=rID)
+
+#確認接單
+@app.route('/accept_food/<int:oID>', methods=['GET','POST'])
+def accept_food(oID):
+    if request.method == 'POST':
+        form = request.form
+        prepareTime = form.get('prepareTime')
+        acceptFood(prepareTime,oID)
+        return redirect(f"/contact_bro/{oID}")
+    
+    return render_template('ContactBro.html', oID=oID)
+
+#我的資料
+@app.route('/aboutME/<int:rID>')
+def aboutME(rID):
+    about = about_me(rID)
+    return render_template('seller/About.html',about=about,rID=rID)
+
+# 修改我的資料
+@app.route('/editME/<int:rID>', methods=['GET', 'POST'])
+def editME(rID):
+    if request.method == 'POST':
+        form = request.form
+        name = form.get('name')
+        email = form.get('email')
+        phone = form.get('phone')
+        address = form.get('address')
+        bank = form.get('bank')
+        time = form.get('time')
+
+        edit_me(name,email,phone,address,bank,time,rID)
+        return redirect(f"/aboutME/{rID}")
+
+    restaurant_info = about_me(rID)
+    return render_template('seller/AboutEdit.html',restaurant_info=restaurant_info, rID=rID)
+
+#我的評價
+@app.route('/review/<int:rID>')
+def review(rID):
+    stars = star(rID)
+    return render_template('seller/Review.html',stars=stars,rID=rID)
